a736e19d7db6d50eeb1781d509de4aba0d29f46a
programmer536 <cg1821mm@go.minnstate.edu>
2026-04-18 03:22:20 -0500

    new
    new

--- src/main/java/Beverage.java
+++ src/main/java/Beverage.java
@@ -1,5 +1,5 @@
-package com.brewbite.model.menu;
 
+package com.brewbite.model.menu;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -97,7 +97,8 @@ public class Beverage extends MenuItem {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Beverage other)) return false;
+        if (!(o instanceof Beverage)) return false;
+        Beverage other = (Beverage) o;
         return super.equals(o) &&
                Objects.equals(beverageType, other.beverageType);
     }
